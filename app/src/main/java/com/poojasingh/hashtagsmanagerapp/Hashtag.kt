dbcce592de3a509f6ac7a30be1ef58d8d064224c
Pooja Singh <84841094+poojasingh1mg@users.noreply.github.com>
2022-09-16 15:24:17 +0530

    Initial commit

---
+++ app/src/main/java/com/poojasingh/hashtagsmanagerapp/Hashtag.kt
@@ -0,0 +1,7 @@
+package com.poojasingh.hashtagsmanagerapp
+
+data class Hashtag (
+    var hashtagName: String? = null,
+    var isHashtagSelected: Boolean = false,
+    var category: String? = null
+    )
